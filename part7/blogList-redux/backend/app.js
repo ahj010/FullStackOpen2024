88ff70e98d3a35f0257c22748dc87ba5389b6fb0
Ali Haider <alihaiderjaffry110@gmail.com>
2025-01-02 04:05:52 +0500

    deployment config

--- part7/blogList-redux/backend/app.js
+++ part7/blogList-redux/backend/app.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === "test") {
   app.use("/api/testing", testingRouter);
 }
 if (process.env.NODE_ENV !== "development") {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3003;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
